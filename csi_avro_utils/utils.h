5876e909963a5e27bd7dfb310b72d1d47c1c2548
skarlsson <svante.karlsson@csi.se>
2015-12-26 17:46:48 +0100

    added get_field_by_name

--- csi_avro_utils/utils.h
+++ csi_avro_utils/utils.h
@@ -5,6 +5,7 @@
 #include <boost/array.hpp>
 #include <avro/Stream.hh>
 #include <avro/ValidSchema.hh>
+#include <avro/Generic.hh>
 
 #pragma once
 
@@ -46,3 +47,51 @@ std::string        to_string(const avro::OutputStream& os);
 boost::uuids::uuid generate_hash(const avro::ValidSchema&);
 std::string        to_string(const avro::ValidSchema& vs);
 std::string        normalize(const avro::ValidSchema&);
+
+
+// should probably go into namespace csi::avro
+// not implemented for 
+// AVRO_NULL (not relevant)
+// AVRO_RECORD 
+// AVRO_ENUM  - dont know how... 
+// AVRO_ARRAY - dont know how... 
+// AVRO_MAP
+// AVRO_UNION
+// AVRO_FIXED
+
+template<typename T> avro::Type CPPToAvroType();
+template<> inline avro::Type CPPToAvroType<std::string>()         { return avro::AVRO_STRING; }
+template<> inline avro::Type CPPToAvroType<std::vector<uint8_t>>(){ return avro::AVRO_BYTES; }
+template<> inline avro::Type CPPToAvroType<int32_t>()             { return avro::AVRO_INT; }
+template<> inline avro::Type CPPToAvroType<int64_t>()             { return avro::AVRO_LONG; }
+template<> inline avro::Type CPPToAvroType<float>()               { return avro::AVRO_FLOAT; }
+template<> inline avro::Type CPPToAvroType<double>()              { return avro::AVRO_DOUBLE; }
+template<> inline avro::Type CPPToAvroType<bool>()                { return avro::AVRO_BOOL; }
+
+// temp code - better names??
+// extracts field by name - either direct member or current branch of union
+// better exceptions
+template<typename T> T get_field_by_name(const avro::GenericDatum& gd, const std::string& field_name)
+{
+    if (gd.type() != avro::AVRO_RECORD)
+        throw std::domain_error(std::string("expected: AVRO_RECORD, actual: ") + avro::toString(gd.type()));
+
+    const avro::GenericRecord& record = gd.value<avro::GenericRecord>();
+    if (!record.hasField(field_name))
+        throw std::domain_error(std::string("no such field: ") + field_name);
+
+    const avro::GenericDatum& field = record.field(field_name);
+
+    if (field.type() == CPPToAvroType<T>())
+        return field.value<T>();
+
+    if (field.isUnion())
+    {
+        const avro::GenericUnion& generic_union = field.value<avro::GenericUnion>();
+        if (generic_union.datum().type() == CPPToAvroType<T>())
+            return generic_union.datum().value<T>();
+        else
+            throw std::bad_cast();
+    }
+    throw std::bad_cast();
+}
